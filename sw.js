3eda84dc2ea35a2481cc12b14fd061f41a9ddd5c
djnastx-a11y <djnastx@gmail.com>
2026-08-19 11:15:09 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="mon-pointage-v6-20260819-1";
+const CACHE="mon-pointage-v7-20260819-1";
 const LOCAL=["./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(LOCAL)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
