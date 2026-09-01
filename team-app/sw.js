8155e6cd594e77acd3cd96f6742d456264ef5fa7
djnastx-a11y <djnastx@gmail.com>
2026-09-02 01:11:47 +0200

    Ajoute le service worker de l’application équipe

---
+++ team-app/sw.js
@@ -0,0 +1,5 @@
+const CACHE="australia-team-v1.0.0";
+const CORE=["./","./index.html","./styles.css","./app.js","./app1.js","./app2.js","./app3.js","./app4.js","./logo.svg","./koala.svg","./manifest.webmanifest","../icon-192.png","../icon-512.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).catch(()=>{}));self.skipWaiting()});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE&&k.startsWith("australia-team-")).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))))});
