a791727b999dc5ed2a6e40cad11eedbe7aff7a5d
djnastx-a11y <djnastx@gmail.com>
2026-09-08 23:57:41 +0200

    Load personal calendar module

--- team-app-next/app.js
+++ team-app-next/app.js
@@ -1,6 +1,6 @@
 (()=>{
-  const VERSION='281';
-  const CORE=['./app1.js','./role-v2.js','./app2.js','./app3.js','./app4.js','./app5.js','./app6.js','./app7.js','./app8.js','./app9.js','./app10.js','./app12.js','./app13.js','./app14.js','./app15.js','./app16.js'];
+  const VERSION='282';
+  const CORE=['./app1.js','./role-v2.js','./app2.js','./app3.js','./app4.js','./app5.js','./app6.js','./app7.js','./app8.js','./app9.js','./app10.js','./app12.js','./app13.js','./app14.js','./app15.js','./app16.js','./app17.js'];
   const loadScript=src=>new Promise((resolve,reject)=>{
     const s=document.createElement('script');s.src=`${src}?v=${VERSION}`;s.onload=resolve;s.onerror=()=>reject(new Error(`Impossible de charger ${src}`));document.body.appendChild(s);
   });
