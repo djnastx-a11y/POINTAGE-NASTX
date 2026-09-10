e27aaf95f656da1b43218bc35580a3acedc501a1
djnastx-a11y <djnastx@gmail.com>
2026-09-10 12:21:50 +0200

    Load planning workflow enhancements

--- team-app-next/app.js
+++ team-app-next/app.js
@@ -1,6 +1,6 @@
 (()=>{
-  const VERSION='300';
-  const CORE=['./app1.js','./role-v2.js','./app2.js','./app3.js','./app4.js','./app5.js','./app6.js','./app7.js','./app8.js','./app9.js','./app10.js','./app12.js','./app13.js','./app14.js','./app15.js','./app16.js','./app17.js','./app20.js','./app21.js','./app22.js','./app23.js','./app24.js'];
+  const VERSION='301';
+  const CORE=['./app1.js','./role-v2.js','./app2.js','./app3.js','./app4.js','./app5.js','./app6.js','./app7.js','./app8.js','./app9.js','./app10.js','./app12.js','./app13.js','./app14.js','./app15.js','./app16.js','./app17.js','./app20.js','./app21.js','./app22.js','./app23.js','./app24.js','./app25.js'];
   const loadScript=src=>new Promise((resolve,reject)=>{
     const s=document.createElement('script');s.src=`${src}?v=${VERSION}`;s.onload=resolve;s.onerror=()=>reject(new Error(`Impossible de charger ${src}`));document.body.appendChild(s);
   });
